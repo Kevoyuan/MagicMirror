13f6f29b2187ee25c9a808faebb8ce48a6aff8a1
Kevoyuan <kevinsyuan@gmail.com>
2023-01-18 23:18:46 +0100

    MagicMirror_rk

--- OpenPosePlugin/Assets/Script/RenderKeypoints.cs
+++ OpenPosePlugin/Assets/Script/RenderKeypoints.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using OpenPose;
 
 namespace MagicMirror
 {
@@ -10,8 +11,10 @@ namespace MagicMirror
     {
 
         // Bone ends
-        public Vector3 endPoint1, endPoint2;
-
+        private Vector3 endPoint1, endPoint2;
+        private OPDatum datum;
+        public int keypointNumber1 = 3;
+        public int keypointNumber2 = 4;
         // Mesh to be rendered
         public Mesh boneMesh;
 
@@ -27,23 +30,47 @@ namespace MagicMirror
         }
 
         // Update is called once per frame
-        void Update()
+        private void Update()
         {
-            if (endPoint1 != Vector3.zero && endPoint2 != Vector3.zero)
+                   // Try getting new frame
+        if (OPWrapper.OPGetOutput(out datum))
+        { // true: has new frame data
+          // Print keypoint 3 to the console
+            int numPoses = datum.poseKeypoints.GetSize(0);
+
+            if (numPoses > 0)
             {
-                // calculate the distance and direction between the two endpoints
-                var distance = Vector3.Distance(endPoint1, endPoint2);
-                var direction = (endPoint2 - endPoint1).normalized;
+                // keypointNumber is located at (0, keypointNumber, 0) and (0, keypointNumber, 1) in the poseKeypoints array
+                float x1 = datum.poseKeypoints.Get(0, keypointNumber1, 0);
+                float y1 = datum.poseKeypoints.Get(0, keypointNumber1, 0);
+                float score1 = datum.poseKeypoints.Get(0, keypointNumber1, 2);
+                Debug.LogFormat("Keypoint {0}: ({1}, {2}), score = {3}", keypointNumber1, x1, y1, score1);
+                float x2 = datum.poseKeypoints.Get(0, keypointNumber2, 0);
+                float y2 = datum.poseKeypoints.Get(0, keypointNumber2, 0);
+                float score2 = datum.poseKeypoints.Get(0, keypointNumber2, 2);
+                Debug.LogFormat("Keypoint {0}: ({1}, {2}), score = {3}", keypointNumber2, x2, y2, score2);
 
-                // position the mesh at the midpoint between the two endpoints
-                transform.position = endPoint1 + direction * distance * 0.5f;
 
-                // rotate the mesh to align with the direction of the two endpoints
-                transform.rotation = Quaternion.LookRotation(direction);
+                endPoint1 = new Vector3(x1, y1, 0);
+                endPoint2 = new Vector3(x2, y2, 0);
 
-                // scale the mesh to match the distance between the two endpoints
-                transform.localScale = new Vector3(1, 1, distance);
+                if (endPoint1 != Vector3.zero && endPoint2 != Vector3.zero)
+                {
+                    // calculate the distance and direction between the two endpoints
+                    var distance = Vector3.Distance(endPoint1, endPoint2);
+                    var direction = (endPoint2 - endPoint1).normalized;
+
+                    // position the mesh at the midpoint between the two endpoints
+                    transform.position = endPoint1 + direction * distance * 0.5f;
+
+                    // rotate the mesh to align with the direction of the two endpoints
+                    transform.rotation = Quaternion.LookRotation(direction);
+
+                    // scale the mesh to match the distance between the two endpoints
+                    transform.localScale = new Vector3(1, 1, distance);
+                }
             }
+        }
         }
     }
 }
